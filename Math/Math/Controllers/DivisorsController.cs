b93576fbad3f2919418a44c0aa13e47b3d3f98f9
Bruno Augusto De Souza Oliveira <bruno.desouza@linx.com.br>
2020-08-04 18:33:16 -0300

    Criada api CalcularNumerosDivisores que retorna uma lista de double

---
+++ Math/Math/Controllers/DivisorsController.cs
@@ -0,0 +1,16 @@
+﻿using Math.Rules;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Math.Controllers
+{
+    public class DivisorsController : Controller
+    {
+        [Route("Divisors/CalcularNumerosDivisores")]
+        [HttpGet]
+        public List<double> CalcularNumerosDivisores(double numero)
+        {
+            return new Divisor().CalcularDivisores(numero);
+        }
+    }
+}
