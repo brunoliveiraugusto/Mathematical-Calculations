126e78a455a6d6e0251ec83bc1a062628b126ddc
Bruno Augusto De Souza Oliveira <bruno.desouza@linx.com.br>
2020-08-05 11:55:12 -0300

    Renomeada api CalcularNumerosDivisores para CalcularNumerosDivisoresENumerosPrimos

---
+++ Math/Math/Controllers/CalculoController.cs
@@ -0,0 +1,16 @@
+﻿using Math.Rules;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Math.Controllers
+{
+    public class CalculoController : Controller
+    {
+        [Route("Calculo/CalcularNumerosDivisoresENumerosPrimos")]
+        [HttpGet]
+        public Calculo CalcularNumerosDivisoresENumerosPrimos(double numero)
+        {
+            return new Calculo().CalcularNumerosDivisoresENumerosPrimos(numero);
+        }
+    }
+}
